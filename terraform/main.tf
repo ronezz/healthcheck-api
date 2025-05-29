478d1c4070029b5ccc2ea1d820b6438bb36f7c1c
ronezz <aaroncelariveiro@gmail.com>
2025-05-29 13:27:58 +0200

    Infraestructura Terraform terminada

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,13 +19,6 @@ resource "aws_security_group" "allow_ssh_http" {
   }
 
   ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
